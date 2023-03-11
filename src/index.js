8aca08dda082e686cc4cc066ddf2fcc7c8c5015e
ilpalbio <ilpalbio@gmail.com>
2023-03-11 01:58:09 +0100

    add methods to createe and add pieces

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ class GameField {
     this.width = 16;
     this.height = 22;
     this.root = document.getElementById("field-container");
+    this.nextPiece = document.getElementById("next-piece-container");
   }
 
   generateField() {
@@ -18,10 +19,62 @@ class GameField {
       }
     }
   }
+
+  // metodo per generare il display per mostrare il prossimo pezzo
+  generateNextMoveField() {
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+        const pixel = document.createElement('div');
+        pixel.setAttribute('id', `${i}-${j}`);
+        pixel.setAttribute('class', 'next-piece-container-pixel');
+
+        this.nextPiece.appendChild(pixel);
+      }
+    }
+  }
+
+  // metodo per creare un pezzo random
+  // 0: lungo azzurro
+  // 1: l sinistra blue
+  // 2: l destra arancione
+  // 3: quadrato giallo
+  // 4: z verde
+  // 5: t viola
+  // 6: z contraria rossa
+  createNextPiece() {
+    const number = Math.round(Math.random() * 6);
+    let piece = null;
+    
+    switch(number) {
+      case 0:
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      case 6:
+        break;
+    }
+
+    return piece;
+  }
+
+  // metodo per aggiungere un pezzo
+  addPiece() {
+    const piece = this.createNextPiece();
+    
+  }
 }
 
 
 window.addEventListener("load", () => {
   const field = new GameField();
   field.generateField();
+  field.generateNextMoveField();
 });
